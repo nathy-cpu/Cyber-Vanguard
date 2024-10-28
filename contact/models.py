4678331e1315f17b3d679c9f234738b016c2ae79
nathy-cpu <degeadavid042@gmail.com>
2024-10-29 02:20:53 +0300

    fixed the git repo url

--- contact/models.py
+++ contact/models.py
@@ -5,8 +5,8 @@ from django.db import models
 
 class ContactMessage(models.Model):
     name = models.CharField(max_length=128)
-    email = models.EmailField()
-    phone_number = models.CharField(max_length=15, blank=True)
+    email = models.EmailField(blank=True)
+    phone_number = models.CharField(max_length=15)
     message = models.TextField()
 
     def __str__(self):
